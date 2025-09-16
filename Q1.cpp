7e585b2f98387e5e87067bc05e3694e0b6fa44de
lakshysingh156 <lakshysingh156@gmail.com>
2025-09-16 17:37:50 +0530

    Add files via upload

--- Q1.cpp
+++ Q1.cpp
@@ -1,55 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Node { int d; Node *next; Node(int x) : d(x), next(NULL) {} };
 int main() {
-    Node *head = NULL;
-    int ch, x, val;
+    queue<int> q;
+    int ch, x;
     while (true) {
-        cout << "\n1 insBeg 2 insEnd 3 delBeg 4 delEnd 5 delVal 6 search 7 display 8 exit\n";
+        cout << "\n1 enq 2 deq 3 empty 4 size 5 front 6 exit\n";
         cin >> ch;
-        if (ch == 1) {
-            cin >> x;
-            Node *p = new Node(x);
-            p->next = head;
-            head = p;
-        }
+        if (ch == 1) { cin >> x; q.push(x); }
         else if (ch == 2) {
-            cin >> x;
-            Node *p = new Node(x);
-            if (!head) head = p;
-            else { Node *t = head; while (t->next) t = t->next; t->next = p; }
+            if (!q.empty()) { cout << q.front() << "\n"; q.pop(); }
+            else cout << "Empty\n";
         }
-        else if (ch == 3) {
-            if (!head) cout << "Empty\n";
-            else { Node *t = head; head = head->next; delete t; }
-        }
-        else if (ch == 4) {
-            if (!head) cout << "Empty\n";
-            else if (!head->next) { delete head; head = NULL; }
-            else { Node *t = head; while (t->next->next) t = t->next; delete t->next; t->next = NULL; }
-        }
-        else if (ch == 5) {
-            cin >> val;
-            if (!head) cout << "Empty\n";
-            else if (head->d == val) { Node *t = head; head = head->next; delete t; }
-            else {
-                Node *t = head;
-                while (t->next && t->next->d != val) t = t->next;
-                if (t->next) { Node *r = t->next; t->next = r->next; delete r; }
-            }
-        }
-        else if (ch == 6) {
-            cin >> val;
-            int pos = 1;
-            Node *t = head;
-            while (t && t->d != val) { t = t->next; pos++; }
-            cout << (t ? pos : -1) << "\n";
-        }
-        else if (ch == 7) {
-            Node *t = head;
-            while (t) { cout << t->d << " "; t = t->next; }
-            cout << "\n";
-        }
+        else if (ch == 3) cout << (q.empty() ? "Empty\n" : "Not Empty\n");
+        else if (ch == 4) cout << q.size() << "\n";
+        else if (ch == 5) cout << (q.empty() ? -1 : q.front()) << "\n";
         else break;
     }
 }
